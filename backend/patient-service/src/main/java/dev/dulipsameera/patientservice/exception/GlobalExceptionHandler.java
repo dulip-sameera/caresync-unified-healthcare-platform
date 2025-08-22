f5495506725324822b6dbf2f7be7a3b7023c494e
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-08-22 09:46:32 +0530

    Fix the missing return statement issue in the general exception handler

--- backend/patient-service/src/main/java/dev/dulipsameera/patientservice/exception/GlobalExceptionHandler.java
+++ backend/patient-service/src/main/java/dev/dulipsameera/patientservice/exception/GlobalExceptionHandler.java
@@ -67,5 +67,7 @@ public class GlobalExceptionHandler {
                 exception.getMessage()
         );
         problemDetail.setTitle("Unexpected Error");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
     }
 }
